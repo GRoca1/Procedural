d45f0ba40e3dee23eaf0b3524c68748948ea0bd1
Georges ROCA <joeroca@hotmail.com>
2020-12-22 09:53:17 +0100

    Kms en Miles
    Exo de clqsse

---
+++ KmEnMiles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmEnMiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Start:
+
+            {
+                Console.WriteLine("Saisissez une valeur en kilomètres:");
+                double kilometres = Convert.ToDouble(Console.ReadLine());
+
+                double miles = kilometres / 1.609;
+
+                Console.WriteLine("La conversion en miles est:");
+                Console.WriteLine(miles + " Miles");
+
+                
+            }
+                Console.WriteLine("Voulez-vous continuer(O/N)?");
+                ConsoleKeyInfo saisie = Console.ReadKey(true);
+            if (saisie.Key == ConsoleKey.O)
+            {              
+                Console.ReadKey();
+                goto Start;
+            }
+            
+            
+            
+
+            
+        }
+
+        
+    }
+}
