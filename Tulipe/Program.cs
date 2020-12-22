52ece58a8afa9ede717062ee832e270d07003f9f
Georges ROCA <joeroca@hotmail.com>
2020-12-22 09:25:41 +0100

    Ajout projet tulipe console
    initiation console

---
+++ Tulipe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 
+{
+    static class Program
+    {
+        Console.WriteLine("Combien d'éléments souhaitez-vous dans le tableau?");
+            int nbrecase = int.Parse(Console.ReadLine());
+        int[] tabNombre = new int[nbrecase];
+            for (int i = 0; i<nbrecase; i++)
+            {
+                Console.WriteLine("Veuillez saisir le nombre numero: {0}/{1}", i + 1, nbrecase);
+                tabNombre[i] = int.Parse(Console.ReadLine());
+    }
+    //fonction qui affiche le tableau...
+
+
+    ManipulationTableau.AfficherTab(tabNombre);
+
+            Console.ReadKey();
+
+        }
+
+    }
+    class ManipulationTableau
+{
+    public static void AfficherTab(int[] _tab)
+    {
+        Console.Write("[ ");
+        foreach (int element in _tab)
+        {
+            Console.Write(element + " ");
+        }
+        Console.Write(" ] \n");
+    }
+
+
+}
+}
